d08275102b570ff9a4cea750fc0742b3d919c9f2
Harineko0 <harineko0927@gmail.com>
2024-03-02 05:18:55 +0900

    fix: fixed BaseArmor.java (replaced EquipmentSlot with ArmorItem.Type)

--- src/main/java/net/fabricmc/majobroom/armors/BaseArmor.java
+++ src/main/java/net/fabricmc/majobroom/armors/BaseArmor.java
@@ -1,16 +1,13 @@
 package net.fabricmc.majobroom.armors;
 
-import net.fabricmc.majobroom.MajoBroom;
 import net.minecraft.block.DispenserBlock;
-import net.minecraft.block.dispenser.DispenserBehavior;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
 
 public class BaseArmor extends DyeableArmorItem implements DyeableItem {
 
-    public BaseArmor(ArmorMaterial material, EquipmentSlot slot) {
-        super(material, slot, new Item.Settings());
+    public BaseArmor(ArmorMaterial material, ArmorItem.Type type) {
+        super(material, type, new Item.Settings());
         DispenserBlock.registerBehavior(this,ArmorItem.DISPENSER_BEHAVIOR);//发射器穿装备
     }
 
